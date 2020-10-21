192e596d99f1b7d88184e654ce3c558a0cf79dd4
nmhummel <natalie.hummel@gmail.com>
2020-10-20 19:18:27 -0500

    commit tree ccb510b8ed142b388121d06390397a8df0acd399

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,22 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
+
+
+
+def speak_to_grandma(phrase)
+    if phrase == 'I LOVE YOU GRANDMA!' # if phrase is equal to 'I LOVE YOU GRANDMA!' then she loves you too
+        'I LOVE YOU TOO PUMPKIN!'
+    elsif phrase == phrase.upcase   #checks for uppercase; if is UPPERCASE, she thinks she heard you and says 1938 phrase
+        'NO, NOT SINCE 1938!'
+    else  # all this is lowercase, so she asks you to SHOUT  
+        'HUH?! SPEAK UP, SONNY!'
+    end
+       
+end
+
+speak_to_grandma("I LOVE YOU GRANDMA!")
+speak_to_grandma("Yo, granny!")
+
